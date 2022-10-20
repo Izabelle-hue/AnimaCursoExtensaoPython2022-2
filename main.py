f9027eb34a946acc2d708ffe709334847b1c9891
Izabelle Hue <82341756+Izabelle-hue@users.noreply.github.com>
2022-10-20 00:54:27 +0000

    I learned to create variables and show them on the screen

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # Meu primeiro projeto Python!!
-#
 # print()= comando de saida
 print("Hello Human")
+#String é um alfa numerico... uma frase..
+nome = "Izabelle Souza"
+idade = 21
+curso = "compute engineering"
+# sem as "" aspas são valores quânticos e tem que ser um número inteiro
+#Exibir o nome (que esta dentro da variável nome)
+print(nome)
+print(idade)
+print(curso)
+      
