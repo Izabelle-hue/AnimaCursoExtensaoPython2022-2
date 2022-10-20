a5d8174360a9f71114a9597abb489d345509628b
Izabelle Hue <82341756+Izabelle-hue@users.noreply.github.com>
2022-10-20 01:19:11 +0000

    Aprendemos de 3 diferentes maneiras de exibir

--- main.py
+++ main.py
@@ -10,4 +10,11 @@ curso = "compute engineering"
 print(nome)
 print(idade)
 print(curso)
-      
+#Para exibir uma frase que completa o conteúdo de uma variável   
+print("Meu nome é "+nome)
+print("Minha idade é "+str(idade))
+print(f"Meu curso é {curso}")
+print("Minha idade é {} anos".format(idade))
+#para não dar erro, na concatenação se usa para converter uma variavel inteiro para string OU DO OUTRO JEITO= print f(Minha idade é{idade})
+#FRASE: Exibir mais de uma varável em uma frase 
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
