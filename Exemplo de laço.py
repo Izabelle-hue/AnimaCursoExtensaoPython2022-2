a077ce9c325aa5bab724e81527596bb351b20907
Izabelle Hue <82341756+Izabelle-hue@users.noreply.github.com>
2022-10-27 01:18:16 +0000

    Exemplo de laço inicio

---
+++ Exemplo de laço.py
@@ -0,0 +1,9 @@
+#Exemplo de laço
+
+#Se eu quiser exibir números de 1 a 10 em topicos:
+#print("1") de amneira rústica # ou numero = 1 do jeito certo
+numero = 1
+print(numero)
+numero = numero + 1
+print (numero) 
+print(numero) 
