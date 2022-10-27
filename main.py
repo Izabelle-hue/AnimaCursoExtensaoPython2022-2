07dff54987302b3f466891bb4906833e5a1bae3b
Izabelle Hue <82341756+Izabelle-hue@users.noreply.github.com>
2022-10-27 00:48:32 +0000

    testes

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 # comando input(): quero permitir que o usuário digite algo...
+
 nome= input("Digite seu nome: ")
+
 #comando de saída.. exibir na tela
+
 idade= int(input("Digite sua idade:"))
 
 #Comando de saída
-print(f"Sua idade é {21}:".format(idade))
-
+print(f"Boa Noite, seu nome é: {nome}")
+#Exibir idade
+print(f"Sua idade é {idade}:".format(idade))
 #Fazer uma equação  de dobro da idade informada
+
 dobro= idade * 2
 print("O dobro da idade informada é {}".format(dobro))
+#Estrutura condicional- o famoso "SE" (if)
+# Se a pessoa for maior de idade, mmostre "Você é maior, ótimo Já pode dirigir"
+if (idade>=18):
+  print("Você é maior de idade Parabéns!! Ja pdoe dirigir")
+else: 
+  print("Você é Xóven ainda, Xóven ainda...")
+  #E se quiser perguntar o gênero (M= Masculino e F= Feminino)
+#Mostre(... Hmm você precisa/precisou prestar o serviço obrigatório)
+
+genero= input("Informe o gênero: M= Maculino; F=Feminino; I= Indefinido:")
+
+if ( idade >= 18 and genero=="M"):
+  print("... Hmm você irá prestar/prestou serviços militares")
+  
+else:
+  print (idade<18 and genero=="M")
+  print("Ta safe até ficar maior de idade "and(genero=="F") )
+  print("Boaaa ta safe")
+
+
+  
+  
+  
+  
