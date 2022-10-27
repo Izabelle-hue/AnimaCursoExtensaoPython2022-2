7c2a5963d2a02e5c72d7a8d5f5625039cd3afd47
Izabelle Hue <82341756+Izabelle-hue@users.noreply.github.com>
2022-10-27 01:08:40 +0000

    Parte 1

---
+++ Parte 02-26-10.py
@@ -0,0 +1,7 @@
+#Pedir o nome do aluno e sua nota (de 0 a 10) e, se ele tirou 10, mostrar"{nome}, "você é bichão mesmo.."
+nome = input("Informe seu nome:")
+RA = input("Informe seu RA")
+nota = float(input("Digite sua nota"))
+if (nota == 10):
+  print(f"{nome}, você é o bichão mesmo...")
+elif (nota==>=7 and )
